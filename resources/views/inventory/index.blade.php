e8933b9475454dadfa2e555cb84f14f4ea417d22
John Chris Cayetano <cayetanojohnchris@gmail.com>
2024-01-13 10:27:27 +0800

    Update index.blade.php

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -109,7 +109,7 @@
                     </div>
                 @endforeach
             </div>
-            {{ $items->withQueryString()->links() }}
+            {{ $items->links() }}
         </div>
     </div>
 @endsection
